ad41374b66043c91c9422eb7548a022bdb6878f7
NduluD <nduluwambua24@gmail.com>
2023-11-02 12:54:16 -0700

    a code that passes the Betty checks

---
+++ task0_simple_shell.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints our names
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("this project is done by Ndulu and Kerich");
+	return (0);
+}
