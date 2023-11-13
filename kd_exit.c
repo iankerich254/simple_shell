525adc34277ef457702c03266aa090ae5643b5fc
iankerich254 <iankerich254@gmail.com>
2023-11-13 10:26:11 -0800

    Add an exit function to simple shell repository

---
+++ kd_exit.c
@@ -0,0 +1,11 @@
+#include "simple_shell_kd.h"
+/**
+ * kd_exit - function that exits the shell
+ * Return: none
+*/
+void kd_exit(void)
+{
+	kd_print("You are now exiting kd_shell$ ");
+	exit(0);
+}
+
